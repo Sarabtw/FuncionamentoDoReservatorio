71cda85d2367f78d8182b885dc08afd2a3e48c18
Sara Monteiro <saramonteiro2008@gmail.com>
2024-10-03 14:49:14 -0300

    Boia

--- Boia/Program.cs
+++ Boia/Program.cs
@@ -1,60 +1,55 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 public class Program
 {
     public static void Main()
     {
-        int boia1, boia2, boia3;
-        bool valvulaLigada, bombaLigada;
-
-        Console.WriteLine("Informe o estado da boia 1 (0 ou 1): ");
+         Console.Write("Informe o estado da Boia 1 (0 ou 1): ");
         boia1 = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("Informe o estado da boia 2 (0 ou 1): ");
+        Console.Write("Informe o estado da Boia 2 (0 ou 1): ");
         boia2 = int.Parse(Console.ReadLine());
-          
-        Console.WriteLine("Informe o estado da boia 3 (0 ou 1): ");
+        Console.Write("Informe o estado da Boia 3 (0 ou 1): ");
         boia3 = int.Parse(Console.ReadLine());
+        
+        if (boia1 == 0 || boia2 == 0 || boia3 == 0) 
+        { 
+            Console.WriteLine("\nLigar válvula e desligar bomba.");
+        } 
+        
+        else if (boia1 == 0 || boia2 == 0 || boia3 == 1) 
+        { 
+            Console.WriteLine("\nLigar válvula e desligar bomba.");
+        } 
+        
+        else if (boia1 == 0 || boia2 == 1 || boia3 == 0) 
+        { 
+            Console.WriteLine("\nFalha na válvula e na bomba.");
+        } 
+        
+        else if (boia1 == 0 || boia2 == 1 || boia3 == 1) 
+        { 
+            Console.WriteLine("\nFalha na válvula e na bomba.");
+        } 
+        
+        else if (boia1 == 1 || boia2 == 0 || boia3 == 0) 
+        { 
+            Console.WriteLine("\nLigar válvula e ligar bomba.");
+        } 
+        
+        else if (boia1 == 1 || boia2 == 0 || boia3 == 1) 
+        { 
+            Console.WriteLine("\nLigar válvula e desligar bomba.");
+        } 
 
-
-        if (boia1 == 0 || boia2 == 0 || boia3 == 0)
-        {
-            Console.WriteLine("Ligar válvula e deligar a bomba.");
+        else if (boia1 == 1 || boia2 == 1 || boia3 == 0) 
+        { 
+            Console.WriteLine("\nDesligar válvula e ligar bomba.");
         }
-
-        if (boia1 == 0 || boia2 == 0 || boia3 == 1)
-        {
-            Console.WriteLine("Ligar válvula e desligar bomba.");
-        }
-
-        if (boia1 == 0 || boia2 == 1 || boia3 == 0)
-        {
-            Console.WriteLine("Falha na válvula ou na bomba.");
-        }
-
-        if (boia1 == 0 || boia2 == 1 || boia3 == 1 )
-        {
-            Console.WriteLine("Falha na válvula ou na bomba.");
-        }
-
-        if(boia1 == 1 || boia2 == 0 || boia3 == 0)
-        {
-            Console.WriteLine("Ligar válvula e ligar bomba.");
-        }
-
-        if(boia1 == 1 || boia2 == 0 || boia3 == 1)
-        {
-            Console.WriteLine("Ligar válvula e desligar bomba.");
-        }
-
-        if(boia1 == 1 || boia2 == 1 || boia3 == 0)
-        {
-            Console.WriteLine("Desligar válvula e ligar bomba.");
-        }
-
-        if (boia1 == 1 || boia2 == 1 || boia3 == 1)
-        {
-            Console.WriteLine("Desligar válvula e desligar bomba.");
+        
+        else if (boia1 == 1 && boia2 == 1 && boia3 == 1) 
+        { 
+            Console.WriteLine("\nDesligar válvula e desligar bomba.");
         }
     }
-}       
+}
